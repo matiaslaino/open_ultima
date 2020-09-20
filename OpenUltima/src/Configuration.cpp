56b955243ffed2148186b260c2b9620605fe2223
Matias <lainomatias@gmail.com>
2020-09-20 04:08:30 +0200

    Update GIF and check if config file exists before starting.

--- OpenUltima/src/Configuration.cpp
+++ OpenUltima/src/Configuration.cpp
@@ -1,7 +1,21 @@
 #include "Configuration.h"
+#include <iostream>
+
+using namespace std;
 
 tao::json::value Configuration::_value;
 
 void Configuration::init() {
     _value = tao::json::parse_file<>("config.json");
+
+    // validate that required configuration entries exist
+    try {
+        Configuration::getMapFilePath();
+        Configuration::getScreenWidth();
+        Configuration::getScreenHeight();
+    } catch (exception &ex) {
+        auto msg = "Error trying to parse configuration file";
+        cout << msg;
+        throw exception(msg);
+    }
 }
